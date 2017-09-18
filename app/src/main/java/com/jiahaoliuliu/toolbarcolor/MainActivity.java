214011b4b94a2d3178d4ae97b107fe900acba9a9
Jiahao Liu <jiahaoliuliu@gmail.com>
2017-09-18 09:11:59 +0400

    First version of the app

---
+++ app/src/main/java/com/jiahaoliuliu/toolbarcolor/MainActivity.java
@@ -0,0 +1,18 @@
+package com.jiahaoliuliu.toolbarcolor;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Find the toolbar view inside the activity layout
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+}
